5c4af70c62d22330e9802657618c922914d55ad8
Tor Bratsberg <tor@designcontainer.no>
2021-04-08 12:37:43 +0200

    Removed some telescope config

--- lua/luafiles/init.lua
+++ lua/luafiles/init.lua
@@ -1,23 +1,3 @@
-local actions = require('telescope.actions')
-require('telescope').setup {
-    defaults = {
-        file_sorter = require('telescope.sorters').get_fzy_sorter,
-        prompt_prefix = ' >',
-        color_devicons = true,
-
-        file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
-        grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
-        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-
-        mappings = {
-            i = {
-                ["<C-x>"] = false,
-                ["<C-q>"] = actions.send_to_qflist,
-            },
-        }
-    },
-}
-
 local M = {}
 
 M.search_notes = function()
